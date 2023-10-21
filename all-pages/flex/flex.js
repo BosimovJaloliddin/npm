4297e6902c638ea6206193bcf42596629bcc829a
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2023-10-21 22:05:28 +0500

    add flex

--- all-pages/flex/flex.js
+++ all-pages/flex/flex.js
@@ -8,6 +8,14 @@ const flexAlignItems = document.querySelector(".flex-align_styles");
 const flexAlignItem = document.querySelectorAll(".flex-align_style");
 const flexItemsTwo = document.querySelector(".flex-items_two");
 
+const small = document.querySelector("#small");
+const middle = document.querySelector("#middle");
+const large = document.querySelector("#large");
+const customize = document.querySelector("#customize");
+const flexItemsThere = document.querySelector(".flex-items_there");
+const flexRange = document.querySelector(".flex-range");
+
+// ========================================
 horizontal.addEventListener("click", (event) => {
   if (event.target.checked) {
     flexItemsOne.classList.remove("vertical");
@@ -39,4 +47,27 @@ flexAlignItem.forEach((value) => {
     flexItemsTwo.style.alignItems = `${target.textContent}`;
     target.classList.add("active");
   });
+});
+
+// ===========================================
+small.addEventListener("click", () => {
+  flexItemsThere.style.gap = "8px";
+
+  flexRange.classList.add("block");
+});
+middle.addEventListener("click", () => {
+  flexItemsThere.style.gap = "18px";
+
+  flexRange.classList.add("block");
+});
+large.addEventListener("click", () => {
+  flexItemsThere.style.gap = "28px";
+
+  flexRange.classList.add("block");
+});
+customize.addEventListener("click", () => {
+  flexRange.classList.remove("block");
+});
+flexRange.addEventListener("input", ({ target }) => {
+  flexItemsThere.style.gap = `${target.value}px`;
 });
