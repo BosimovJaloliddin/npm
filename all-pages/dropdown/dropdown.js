427f8ebbaa6bdb4c5fa9e6d10477575506870e85
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2023-10-23 15:48:18 +0500

    dropdown

--- all-pages/dropdown/dropdown.js
+++ all-pages/dropdown/dropdown.js
@@ -1,36 +1,12 @@
 const dropdownOneBtns = document.querySelectorAll(".dropdown-one_btn");
+const dropdownOneResult = document.querySelector(".dropdown-one_result");
 const dropdownOneHover = document.querySelector(".dropdown-one_hover");
 const dropdownOneItem = document.querySelector(".dropdown-one_item");
 const dropdownOneArrow = document.querySelector(".dropdown-one_arrow");
 const dropdownOneItems = document.querySelector(".dropdown-one_items");
 
-dropdownOneBtns.forEach((btn) => {
-  btn.addEventListener("mouseover", () => {
-    if (btn.getAttribute("disabled")) {
-      btn.style.cursor = `no-drop`;
-    } else if (btn.getAttribute("class").includes("red")) {
-      btn.style.cssText = `
-        color: #ffffff;
-        background-color: var(--error-main);
-      `;
-    } else {
-      btn.style.cssText = `
-        color: #ffffff;
-        background-color: var(--primary-light);
-      `;
-    }
-  });
-  btn.addEventListener("mouseout", () => {
-    if (btn.getAttribute("class").includes("red")) {
-      btn.style.cssText = `
-        color: var(--error-main));
-        background-color: transparent;
-      `;
-    } else if (!btn.getAttribute("disabled")) {
-      btn.style.cssText = `
-        color: var( --text-primary);
-        background-color: transparent;
-      `;
-    }
+dropdownOneBtns.forEach((element) => {
+  element.addEventListener("click", ({ target: { textContent } }) => {
+    dropdownOneResult.textContent = `${textContent}`;
   });
 });
