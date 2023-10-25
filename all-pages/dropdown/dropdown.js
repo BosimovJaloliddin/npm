4a3158e8da5f907ab7a8064f988ec570344b4882
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2023-10-25 17:05:42 +0500

    add dropdown

--- all-pages/dropdown/dropdown.js
+++ all-pages/dropdown/dropdown.js
@@ -1,12 +1,30 @@
 const dropdownOneBtns = document.querySelectorAll(".dropdown-one_btn");
 const dropdownOneResult = document.querySelector(".dropdown-one_result");
-const dropdownOneHover = document.querySelector(".dropdown-one_hover");
-const dropdownOneItem = document.querySelector(".dropdown-one_item");
-const dropdownOneArrow = document.querySelector(".dropdown-one_arrow");
-const dropdownOneItems = document.querySelector(".dropdown-one_items");
 
+const dropdownTwoBtns = document.querySelectorAll(".dropdown-two_btn");
+const dropdownTwoItem = document.querySelector(".dropdown-two_item");
+const dropdownTwoItems = document.querySelector(".dropdown-two_items");
+const dropdownTwoHover = document.querySelector(".dropdown-two_hover");
+const dropdownTwoResult = document.querySelector(".dropdown-two_result");
+
+// ====================================
 dropdownOneBtns.forEach((element) => {
   element.addEventListener("click", ({ target: { textContent } }) => {
     dropdownOneResult.textContent = `${textContent}`;
   });
+});
+
+// =================================
+dropdownTwoBtns.forEach((element) => {
+  element.addEventListener("click", ({ target: { textContent } }) => {
+    dropdownTwoResult.textContent = `${textContent}`;
+  });
+});
+dropdownTwoItem.addEventListener("click", () => {
+  dropdownTwoHover.classList.toggle("active");
+});
+window.addEventListener("click", (event) => {
+  if (event.target !== dropdownTwoItem && event.target !== dropdownTwoHover) {
+    dropdownTwoHover.classList.remove("active");
+  }
 });
