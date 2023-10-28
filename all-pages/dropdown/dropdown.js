bcf0c2e708c6825c76c1cee353e7785e5d31295a
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2023-10-28 21:57:54 +0500

    add dropdown

--- all-pages/dropdown/dropdown.js
+++ all-pages/dropdown/dropdown.js
@@ -3,9 +3,12 @@ const dropdownOneResult = document.querySelector(".dropdown-one_result");
 
 const dropdownTwoBtns = document.querySelectorAll(".dropdown-two_btn");
 const dropdownTwoItem = document.querySelector(".dropdown-two_item");
-const dropdownTwoItems = document.querySelector(".dropdown-two_items");
 const dropdownTwoHover = document.querySelector(".dropdown-two_hover");
 const dropdownTwoResult = document.querySelector(".dropdown-two_result");
+const dropdownTwoArrow = document.querySelector(".dropdown-two_arrow");
+
+const dropdownThereBar = document.querySelector(".dropdown-there_menu");
+const dropdownThereHover = document.querySelector(".dropdown-there_hover");
 
 // ====================================
 dropdownOneBtns.forEach((element) => {
@@ -14,7 +17,7 @@ dropdownOneBtns.forEach((element) => {
   });
 });
 
-// =================================
+// ==============  Two  ===================
 dropdownTwoBtns.forEach((element) => {
   element.addEventListener("click", ({ target: { textContent } }) => {
     dropdownTwoResult.textContent = `${textContent}`;
@@ -22,9 +25,23 @@ dropdownTwoBtns.forEach((element) => {
 });
 dropdownTwoItem.addEventListener("click", () => {
   dropdownTwoHover.classList.toggle("active");
+
+  if (dropdownTwoHover.classList.contains("active")) {
+    dropdownTwoArrow.style.transform = `rotate(90deg)`;
+  } else {
+    dropdownTwoArrow.style.transform = `rotate(0deg)`;
+  }
 });
 window.addEventListener("click", (event) => {
   if (event.target !== dropdownTwoItem && event.target !== dropdownTwoHover) {
     dropdownTwoHover.classList.remove("active");
   }
 });
+
+// ================ There ===============
+// dropdownThereBar.addEventListener("mouseover", () => {
+//   dropdownThereHover.classList.add("active");
+// });
+// dropdownThereBar.addEventListener("mouseout", () => {
+//   dropdownThereHover.classList.remove("active");
+// });
